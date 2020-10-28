b5bcf3c5b74e6065fcc0e569e6f718b7c1e79712
KinggodChanJong <smallman0810@gmail.com>
2020-10-28 14:35:12 +0900

    코로나체크 - qr체크인 부분 수정

--- app/src/main/java/com/kang/coronacheck1/QrcodeActivity.java
+++ app/src/main/java/com/kang/coronacheck1/QrcodeActivity.java
@@ -6,10 +6,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,23 +17,22 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.nhn.android.naverlogin.OAuthLogin;
 
 import java.io.File;
 
-public class QrcodeActivity extends AppCompatActivity {
+public class QrcodeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "로그";
 
-    private static String OAUTH_CLIENT_ID = "dJ5BmNbAdzAmyc8Jczwj";
-    private static String OAUTH_CLIENT_SECRET = "iOpZejA0pc";
-    private static String OAUTH_CLIENT_NAME = "코로나 체크";
-
-
     public static Context mContext;//새로고침을 위한 추가
-
 
     private WebView mWebView;
     private WebSettings mWebSettings;
+    private Button btnHome, btnSetting;
+
+    Intent intent;
 
 
     @Override
@@ -43,7 +42,10 @@ public class QrcodeActivity extends AppCompatActivity {
 
         Log.d(TAG, "QrcodeActivity - onCreate() called");
 
-
+        btnHome = (Button)findViewById(R.id.btn_qr_home);
+        btnSetting = (Button)findViewById(R.id.btn_qr_setting);
+        btnSetting.setOnClickListener(this);
+        btnHome.setOnClickListener(this);
         mWebView = (WebView) findViewById(R.id.webView);
         mWebView.setWebViewClient(new WebViewClient());
         mWebSettings = mWebView.getSettings();
@@ -62,125 +64,72 @@ public class QrcodeActivity extends AppCompatActivity {
         mWebView.loadUrl("https://nid.naver.com/login/privacyQR/");
 
 
+        /**
         Button btn11 = (Button)findViewById(R.id.btn_logout11);
         btn11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //clearApplicationData(this);
+
                 mWebView.clearCache(true);
+                mWebView.clearCache(true);
+                mWebView.clearHistory();
+                // 자동완성은 8.0부터는 내장되어 아래 함수 안먹음
+                mWebView.clearFormData();
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    CookieSyncManager cookieSyncMngr=CookieSyncManager.createInstance(QrcodeActivity.this);
+                    cookieSyncMngr.startSync();
+                    CookieManager cookieManager=CookieManager.getInstance();
+                    cookieManager.removeAllCookie();
+                    cookieManager.removeSessionCookie();
+                    cookieSyncMngr.stopSync();
+                    cookieSyncMngr.sync();
+                }else {
+                    CookieManager cookieManager = CookieManager.getInstance();
+                    cookieManager.removeAllCookies(new ValueCallback() {
+                        @Override
+                        public void onReceiveValue(Object value) {
+
+                        }
+                    });
+                    cookieManager.getInstance().flush();
+                }
                 Intent i = new Intent(QrcodeActivity.this, MainActivity.class);
                 startActivity(i);
-                finish();
             }
         });
+        **/
+
     }
 
-    public void clearApplicationData(View.OnClickListener onClickListener) {
-        mWebView.clearCache(true);
-        mWebView.clearHistory();
-        // 자동완성은 8.0부터는 내장되어 아래 함수 안먹음
-        mWebView.clearFormData();
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            CookieSyncManager cookieSyncMngr=CookieSyncManager.createInstance(this);
-            cookieSyncMngr.startSync();
-            CookieManager cookieManager=CookieManager.getInstance();
-            cookieManager.removeAllCookie();
-            cookieManager.removeSessionCookie();
-            cookieSyncMngr.stopSync();
-            cookieSyncMngr.sync();
-        }else {
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.removeAllCookies(new ValueCallback() {
-                @Override
-                public void onReceiveValue(Object value) {
-                    Log.d("onReceiveValue", value.toString());
-                }
-
-            });
-            cookieManager.getInstance().flush();
-        }
-    }
-
-    private static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
+    public boolean deleteCache(File dir){
+        //File이 Null 인지 Null이 아니라면 폴더인지 체크
+        if(dir != null && dir.isDirectory()){
+            //Null도 아니고 폴더도 아니라면 폴더안 파일 리스트를 호출
             String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
+            //파일 리스트를 반복문으로 호출
+            for(String child : children){
+                //파일 리스트중 폴더가 존재할 수 있기 때문에 재귀호출
+                boolean isSuccess = deleteCache(new File(dir, child));
+                if(!isSuccess){
                     return false;
                 }
             }
         }
         return dir.delete();
     }
-    /**
-    private void clearApplicationCache(){
-        File dir = getCacheDir();
-        if(dir==null) return;
 
-        File[] children = dir.listFiles();
-        try {
-            // 쿠키 삭제
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.removeSessionCookie();
-
-            for(int i=0;i<children.length;i++) {
-                if(children[i].isDirectory()) {
-                    clearApplicationCache(children[i]);
-                } else {
-                    children[i].delete();
-                }
-            }
-        } catch(Exception e){}
-    }
-
-    private void clearApplicationCache(java.io.File dir){
-        if(dir==null) dir = getCacheDir();
-        if(dir==null) return;
-
-        java.io.File[] children = dir.listFiles();
-        try{
-            for(int i=0;i<children.length;i++)
-                if(children[i].isDirectory()) {
-                    clearApplicationCache(children[i]);
-                } else {
-                    children[i].delete();
-                }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_qr_home :
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_qr_setting:
+                intent = new Intent(getApplicationContext(), SettingMain.class);
+                startActivity(intent);
+                break;
         }
-        catch(Exception e){}
     }
-
-
-
-    public static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-
-        return dir.delete();
-    }
-/**
-    public boolean deleteDir() {
-        File cache = getCacheDir();
-        File appDir = new File(cache.getParent());
-        if (appDir != null && appDir.isDirectory()) {
-            String[] children = appDir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir();
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        return appDir.delete();
-    }
-**/
-
 }
